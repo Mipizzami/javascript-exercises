808c552a58ce44dd1f53038222bb7820d9285f28
Mipizzami <167435216+Mipizzami@users.noreply.github.com>
2024-10-31 17:18:07 +0100

    Do all exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahr) {
+  let Cels = (Fahr - 32) * (5/9);
+  const rounded = Math.round(Cels *10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Cels) {
+  let Fahr = (Cels * (9/5) + 32);
+  const rounded = Math.round(Fahr * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
